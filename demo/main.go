cb4b254025a92b9f4dbd1263176cabbe577b9423
larry.liu <jclarry@qq.com>
2019-05-27 19:16:42 +0800

    增加HTTPS方式

--- demo/main.go
+++ demo/main.go
@@ -14,9 +14,14 @@ func main() {
 	engine.GET("/", func(context *gin.Context) {
 		context.String(200, "hello world")
 	})
+	//生成pem和key
+	//openssl req -new -nodes -x509 -out server.pem -keyout server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.egcode.cn/emailAddress=jclarry@qq.com"
 	if err := httpserver.NewServer(httpserver.Config{
 		ListenAddr: ":8080",
 		Handler:    engine,
+		EnableHttps:true,
+		KeyFile:"./server.key",
+		CertFile:"./server.pem",
 	}); err != nil {
 		panic(err)
 	}
