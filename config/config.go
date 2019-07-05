e169f32960ab563c22c0e8965ef21577cc33b7ea
larry.liu <jclarry@qq.com>
2019-07-05 18:16:38 +0800

    修改配置文件

--- config/config.go
+++ config/config.go
@@ -1,8 +1,8 @@
 package config
 
 import (
-	"github.com/larry-dev/plugins/eglog"
 	"github.com/fsnotify/fsnotify"
+	"github.com/larry-dev/plugins/eglog"
 	"github.com/spf13/viper"
 	"strings"
 )
@@ -10,17 +10,19 @@ import (
 type Config struct {
 	Name      string
 	EnvPrefix string
+	Type      string
+	Watch     bool
 }
 
-func Init(cfg string, options ...string) error {
-	prefix := "EG"
-	if len(options) > 0 {
-		prefix = options[0]
+// DefaultConfig 默认配置
+func DefaultConfig() *Config {
+	return &Config{
+		EnvPrefix: "EG",
+		Type:      "yaml",
+		Watch:     true,
 	}
-	c := Config{
-		Name:      cfg,
-		EnvPrefix: prefix,
-	}
+}
+func Init(c *Config) error {
 
 	// 初始化配置文件
 	if err := c.initConfig(); err != nil {
@@ -28,8 +30,9 @@ func Init(cfg string, options ...string) error {
 	}
 
 	// 监控配置文件变化并热加载程序
-	c.watchConfig()
-
+	if c.Watch {
+		c.watchConfig()
+	}
 	return nil
 }
 
@@ -40,7 +43,7 @@ func (c *Config) initConfig() error {
 		viper.AddConfigPath("conf") // 如果没有指定配置文件，则解析默认的配置文件
 		viper.SetConfigName("config")
 	}
-	viper.SetConfigType("yaml")     // 设置配置文件格式为YAML
+	viper.SetConfigType(c.Type)     // 设置配置文件格式为YAML
 	viper.AutomaticEnv()            // 读取匹配的环境变量
 	viper.SetEnvPrefix(c.EnvPrefix) // 读取环境变量的前缀为EG
 	replacer := strings.NewReplacer(".", "_")
